5757e68643c9042e93a65e7368e201c532b10dfe
Brian Stock <bestock@uwaterloo.ca>
2014-03-29 20:19:56 -0400

    UpdateManagerActivity is born
    UpdateManagerActivity is the first activity which is launched with the
    app.

---
+++ gen/ca/uwaterloo/syde/shipmate/UpdateManagerActivity.java
@@ -0,0 +1,16 @@
+package ca.uwaterloo.syde.shipmate;
+
+import android.app.Activity;
+import android.os.Bundle;
+import ca.uwaterloo.syde.shipmate.R;
+
+public class UpdateManagerActivity extends Activity {
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.update_manager_layout);
+	}
+
+}
