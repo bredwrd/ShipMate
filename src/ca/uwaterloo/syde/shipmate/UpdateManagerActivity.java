506a30f4dd9f9629d85b93e75c34c92f3f4356db
Brian Stock <bestock@uwaterloo.ca>
2014-03-29 22:31:51 -0400

    Began implementing UpdateManagerActivity
    also added stub methods to DataUpdaterService

--- src/ca/uwaterloo/syde/shipmate/UpdateManagerActivity.java
+++ src/ca/uwaterloo/syde/shipmate/UpdateManagerActivity.java
@@ -2,15 +2,26 @@ package ca.uwaterloo.syde.shipmate;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 import ca.uwaterloo.syde.shipmate.R;
+import ca.uwaterloo.syde.shipmate.control.DataUpdaterService;
 
 public class UpdateManagerActivity extends Activity {
+	
+	TextView updateStatusText;
+	DataUpdaterService dataUpdaterService = new DataUpdaterService();
 
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
 	    setContentView(R.layout.update_manager_layout);
+	    displayMessage("Loading...");
+	    dataUpdaterService.updateNodeSources();
 	}
-
+	
+	private void displayMessage(String message) {
+		TextView updateStatusText = (TextView) findViewById(R.layout.update_manager_layout);
+		updateStatusText.setText(message);
+	}
 }
--- src/ca/uwaterloo/syde/shipmate/control/DataUpdaterService.java
+++ src/ca/uwaterloo/syde/shipmate/control/DataUpdaterService.java
@@ -5,7 +5,12 @@ import ca.uwaterloo.syde.shipmate.entity.NodeDatabase;
 public class DataUpdaterService {
 	private final String deliveryStandardsDocumentUrl = "http://www.canadapost.ca/tools/pg/manual/pgservstds-e.asp";
 	private NodeDatabase nodeDb;
+	
 	public DataUpdaterService() {
+		
+	}
+	
+	public void updateNodeSources() {
 		
 	}
 }
