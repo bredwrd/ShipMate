53733668ec6b6f3fb1d52672e932345f8fc4323d
FrankAlfieri <alfieri.a.frank@gmail.com>
2014-04-01 14:10:31 -0400

    Added some stuff to DSLM

--- src/ca/uwaterloo/syde/shipmate/HomeActivity.java
+++ src/ca/uwaterloo/syde/shipmate/HomeActivity.java
@@ -4,6 +4,7 @@ import ca.uwaterloo.syde.shipmate.control.DataUpdaterService;
 import ca.uwaterloo.syde.shipmate.control.DeliveryStandardCalculator;
 import ca.uwaterloo.syde.shipmate.control.DomesticLettermailDeliveryStandardCalculator;
 import ca.uwaterloo.syde.shipmate.control.GeocoderController;
+import ca.uwaterloo.syde.shipmate.entity.PostalCode;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -11,7 +12,7 @@ import android.widget.TextView;
 public class HomeActivity extends Activity {
 	//TODO TextView updateStatusText;
 	DataUpdaterService dataUpdaterService = new DataUpdaterService();
-	DeliveryStandardCalculator deliveryStandardCalculator = new DomesticLettermailDeliveryStandardCalculator();
+	DeliveryStandardCalculator deliveryStandardCalculator;
 	//TODO new....
 	GeocoderController geocodercontroller;
 	public void onCreate(Bundle bundle) {
--- src/ca/uwaterloo/syde/shipmate/control/DomesticLettermailDeliveryStandardCalculator.java
+++ src/ca/uwaterloo/syde/shipmate/control/DomesticLettermailDeliveryStandardCalculator.java
@@ -1,11 +1,21 @@
 package ca.uwaterloo.syde.shipmate.control;
 
-public class DomesticLettermailDeliveryStandardCalculator implements DeliveryStandardCalculator {
+import ca.uwaterloo.syde.shipmate.entity.PostalCode;
 
-	@Override
+public class DomesticLettermailDeliveryStandardCalculator implements DeliveryStandardCalculator {
+	private PostalCode origin;
+	private PostalCode destination;
+	private int deliveryStandard;
+	
+	public DomesticLettermailDeliveryStandardCalculator(PostalCode origin, PostalCode destination) {
+		this.origin = origin;
+		this.destination = destination;
+	}
+	
 	public int getDeliveryStandard() {
 		// TODO Auto-generated method stub
+		// Do stuff with origin, destination PostalCodes and return delivery standard, in business days
 		return 0;
 	}
-
+	
 }
