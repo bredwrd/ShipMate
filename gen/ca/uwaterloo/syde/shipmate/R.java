96009926d0e27fc3061e61a4e4efd4137d16d5dd
BRIAN WILDSMITH <smitty.s.ipod@gmail.com>
2014-04-01 14:14:45 -0400

    added text to strings file for...cohesion

--- gen/ca/uwaterloo/syde/shipmate/R.java
+++ gen/ca/uwaterloo/syde/shipmate/R.java
@@ -14,18 +14,18 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int address_text=0x7f060002;
-        public static final int get_address_button=0x7f060001;
-        public static final int location_text=0x7f060000;
-        public static final int updateStatusProgress=0x7f060003;
-        public static final int updateStatusText=0x7f060004;
+        public static final int updateStatusProgress=0x7f060000;
+        public static final int updateStatusText=0x7f060001;
     }
     public static final class layout {
         public static final int home_layout=0x7f030000;
         public static final int update_manager_layout=0x7f030001;
     }
     public static final class string {
+        public static final int address_text=0x7f040003;
         public static final int app_name=0x7f040000;
+        public static final int get_address_button=0x7f040002;
+        public static final int location_text=0x7f040001;
     }
     public static final class style {
         /** 
