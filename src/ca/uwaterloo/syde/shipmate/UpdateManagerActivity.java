b3dc3ae9bd9751c7c6c9075b25e3c3a9bf98fbb8
Brian Stock <bestock@uwaterloo.ca>
2014-04-01 01:22:10 -0400

    Default Activity set
    When the app is launched, create UpdateManagerActivity.

--- src/ca/uwaterloo/syde/shipmate/UpdateManagerActivity.java
+++ src/ca/uwaterloo/syde/shipmate/UpdateManagerActivity.java
@@ -4,11 +4,11 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
 import ca.uwaterloo.syde.shipmate.R;
+
 import ca.uwaterloo.syde.shipmate.control.DataUpdaterService;
 
 public class UpdateManagerActivity extends Activity {
-	
-	TextView updateStatusText;
+
 	DataUpdaterService dataUpdaterService = new DataUpdaterService();
 
 	/** Called when the activity is first created. */
@@ -21,7 +21,7 @@ public class UpdateManagerActivity extends Activity {
 	}
 	
 	private void displayMessage(String message) {
-		TextView updateStatusText = (TextView) findViewById(R.layout.update_manager_layout);
+		TextView updateStatusText = (TextView) findViewById(R.id.updateStatusText);
 		updateStatusText.setText(message);
 	}
 }
