54aad7e6feca0f8370bb72192e1b2410eefe0d97
Brian Stock <bestock@uwaterloo.ca>
2014-04-03 16:34:23 -0400

    Implemented HomeActivity Run (submit) button click listener

--- src/ca/uwaterloo/syde/shipmate/HomeActivity.java
+++ src/ca/uwaterloo/syde/shipmate/HomeActivity.java
@@ -2,10 +2,13 @@ package ca.uwaterloo.syde.shipmate;
 
 import ca.uwaterloo.syde.shipmate.control.DataUpdaterService;
 import ca.uwaterloo.syde.shipmate.control.DeliveryStandardCalculator;
+import ca.uwaterloo.syde.shipmate.control.DomesticLettermailDeliveryStandardCalculator;
 import ca.uwaterloo.syde.shipmate.control.GPSTracker;
+import ca.uwaterloo.syde.shipmate.entity.PostalCode;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class HomeActivity extends Activity {
@@ -17,7 +20,17 @@ public class HomeActivity extends Activity {
 	public void onCreate(Bundle bundle) {
 		super.onCreate(bundle);
 		setContentView(R.layout.home_layout);
-		//Button runButton = findViewById();
+		Button runButton = (Button) findViewById(R.id.run_button);
+		runButton.setOnClickListener(new Button.OnClickListener() {  
+	        public void onClick(View v)
+            {
+	        	EditText toPostalCodeField = (EditText) findViewById(R.id.to_postal_code);
+	        	EditText fromPostalCodeField = (EditText) findViewById(R.id.from_postal_code);
+	        	PostalCode toPostalCode = new PostalCode(toPostalCodeField.getText().toString());
+	        	PostalCode fromPostalCode = new PostalCode(fromPostalCodeField.getText().toString());
+	        	deliveryStandardCalculator = new DomesticLettermailDeliveryStandardCalculator(toPostalCode, fromPostalCode);
+            }
+         });
 	}
 	public void AddressButton_OnClick(View v) {
 		gpsTracker.getLocation();
