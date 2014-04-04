c74abb20139a5eb1866053fc37c3789f94507b6a
Brian Stock <bestock@uwaterloo.ca>
2014-04-04 18:47:09 -0400

    Logging objects are fleshed out

--- src/ca/uwaterloo/syde/shipmate/entity/LogDatabase.java
+++ src/ca/uwaterloo/syde/shipmate/entity/LogDatabase.java
@@ -1,11 +1,29 @@
 package ca.uwaterloo.syde.shipmate.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.database.sqlite.SQLiteOpenHelper;
 
+/**
+ * LogDatabase manages historic log data.
+ * @author Brian
+ *
+ */
 public class LogDatabase extends SQLiteOpenHelper {
+	
+	private static final String TABLE_LOG =  "Log";
+	private static final String KEY_ID = "Key";
+	private static final String KEY_ORIGIN = "Origin";
+	private static final String KEY_DESTINATION = "Destination";
+	private static final String KEY_SHIPPING_TYPE="ShippingType";
+	private static final String KEY_DATE="Date";
+	private static final String KEY_DELIVERY_STANDARD = "DELIVERY STANDARD";
 
 	public LogDatabase(Context context, String name, CursorFactory factory,
 			int version) {
@@ -15,19 +33,67 @@ public class LogDatabase extends SQLiteOpenHelper {
 
 	@Override
 	public void onCreate(SQLiteDatabase db) {
-        String CREATE_CONTACTS_TABLE = "CREATE TABLE " + TABLE_CONTACTS + "("
-                + KEY_ID + " INTEGER PRIMARY KEY," + KEY_NAME + " TEXT,"
-                + KEY_PH_NO + " TEXT" + ")";
+        String CREATE_CONTACTS_TABLE = "CREATE TABLE " + TABLE_LOG + "("
+                + KEY_ID + " INTEGER PRIMARY KEY," + KEY_ORIGIN + " TEXT,"
+                + KEY_DESTINATION + " TEXT," + KEY_SHIPPING_TYPE + " TEXT," + KEY_DELIVERY_STANDARD + KEY_DATE + " TEXT" + ")";
         db.execSQL(CREATE_CONTACTS_TABLE);
 	}
 
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         // Drop older table if existed
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_CONTACTS);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_LOG);
  
         // Create tables again
         onCreate(db);
 	}
+	
+    // Adding new contact
+    void addEntry(LogEntry logEntry) {
+        SQLiteDatabase db = this.getWritableDatabase();
+ 
+        // Define stored values
+        ContentValues values = new ContentValues();
+        values.put(KEY_ORIGIN, logEntry.getOrigin());
+        values.put(KEY_DESTINATION, logEntry.getDestination());
+        values.put(KEY_SHIPPING_TYPE, logEntry.getShippingType());
+        values.put(KEY_DELIVERY_STANDARD, logEntry.getDeliveryStandard());
+ 
+        // Inserting Row
+        db.insert(TABLE_LOG, null, values);
+        db.close(); // Closing database connection
+    }
+     
+    // Getting All Contacts
+    public List<LogEntry> getAllEntries() {
+        List<LogEntry> entryList = new ArrayList<LogEntry>();
+        // Select All Query
+        String selectQuery = "SELECT  * FROM " + TABLE_LOG;
+ 
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+ 
+        // looping through all rows and adding to list
+        if (cursor.moveToFirst()) {
+            do {
+                LogEntry logEntry = new LogEntry(cursor.getString(0), cursor.getString(1), cursor.getString(2), cursor.getString(3), cursor.getString(4));
+                // Adding entry to list
+                entryList.add(logEntry);
+            } while (cursor.moveToNext());
+        }
+        // return contact list
+        return entryList;
+    } 
+ 
+    // Getting contacts Count
+    public int getContactsEntries() {
+        String countQuery = "SELECT  * FROM " + TABLE_LOG;
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(countQuery, null);
+        cursor.close();
+ 
+        // return count
+        return cursor.getCount();
+    }
 
 }
---
+++ src/ca/uwaterloo/syde/shipmate/entity/LogEntry.java
@@ -0,0 +1,52 @@
+package ca.uwaterloo.syde.shipmate.entity;
+
+public class LogEntry {
+
+	private String origin;
+	private String destination;
+	private String shippingType;
+	private String date;
+	private String deliveryStandard;
+	
+	public LogEntry(String origin, String destination, String shippingType, String date, String deliveryStandard) {
+		this.origin = origin;
+		this.destination = destination;
+		this.shippingType = shippingType;
+		this.date = date;
+		this.deliveryStandard = deliveryStandard;
+	}
+	
+	public String getOrigin() {
+		return origin;
+	}
+	public void setOrigin(String origin) {
+		this.origin = origin;
+	}
+	public String getDestination() {
+		return destination;
+	}
+	public void setDestination(String destination) {
+		this.destination = destination;
+	}
+	public String getShippingType() {
+		return shippingType;
+	}
+	public void setShippingType(String shippingType) {
+		this.shippingType = shippingType;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getDeliveryStandard() {
+		return deliveryStandard;
+	}
+	public void setDeliveryStandard(String deliveryStandard) {
+		this.deliveryStandard = deliveryStandard;
+	}
+
+	
+	
+}
