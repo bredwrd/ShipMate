a8677528a6a354b93dfb4a8a6fca6b451f83a849
FrankAlfieri <alfieri.a.frank@gmail.com>
2014-04-03 21:37:13 -0400

    Threaded gps location

--- src/ca/uwaterloo/syde/shipmate/HomeActivity.java
+++ src/ca/uwaterloo/syde/shipmate/HomeActivity.java
@@ -6,16 +6,20 @@ import ca.uwaterloo.syde.shipmate.control.DomesticLettermailDeliveryStandardCalc
 import ca.uwaterloo.syde.shipmate.control.GPSTracker;
 import ca.uwaterloo.syde.shipmate.entity.PostalCode;
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 
 public class HomeActivity extends Activity {
 	//TODO TextView updateStatusText;
-	DataUpdaterService dataUpdaterService = new DataUpdaterService();
-	DeliveryStandardCalculator deliveryStandardCalculator;
-	GPSTracker gpsTracker;
+	private DataUpdaterService dataUpdaterService = new DataUpdaterService();
+	private DeliveryStandardCalculator deliveryStandardCalculator;
+	private GPSTracker gpsTracker;
+	private ProgressBar progress;
+	private EditText editText;
 	
 	public void onCreate(Bundle bundle) {
 		super.onCreate(bundle);
@@ -35,19 +39,35 @@ public class HomeActivity extends Activity {
             }
          });
 	}
-	public void AddressButton_OnClick(View v) {
-		gpsTracker.getLocation();
-		gpsTracker.getLongitude();
-		gpsTracker.getLatitude();
-		String address_text = gpsTracker.getGeocoderAddress(this).get(0).getPostalCode();
-		if (v.getId() == R.id.to_button){
-			EditText tv2 = (EditText)findViewById(R.id.to_postal_code);
-			tv2.setText(address_text);
-			
-		} else if (v.getId() == R.id.from_button){
-			EditText tv2 = (EditText)findViewById(R.id.from_postal_code);
-			tv2.setText(address_text);
-		}
-		
+	public void AddressButton_OnClick(final View v) {
+		final Context context = this;
+		Thread thread = new Thread(){
+			String address_text;
+	        public void run() {
+				gpsTracker.getLocation();
+				gpsTracker.getLongitude();
+				gpsTracker.getLatitude();
+			    address_text = gpsTracker.getGeocoderAddress(context).get(0).getPostalCode();
+	        	runOnUiThread(new Runnable() {
+					public void run() {
+						//Set text
+						editText.setText(address_text);
+						//stop spinning wheel
+				        progress.setVisibility(View.INVISIBLE);
+					}
+	            });
+	        }
+        };
+        if (v.getId() == R.id.to_button){
+			editText = (EditText)findViewById(R.id.to_postal_code);
+        	progress = (ProgressBar) findViewById(R.id.to_gpsStatusProgress);
+        	
+        } else if (v.getId() == R.id.from_button){
+			editText = (EditText)findViewById(R.id.from_postal_code);
+        	progress = (ProgressBar) findViewById(R.id.from_gpsStatusProgress);
+        }
+		// start spinning wheel
+        progress.setVisibility(View.VISIBLE);
+        thread.start();
 	}
 }
