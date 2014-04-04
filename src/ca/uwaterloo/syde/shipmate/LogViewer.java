2df1afb950e59011f14656943be8bb16947dbc12
Brian Stock <bestock@uwaterloo.ca>
2014-04-04 16:35:23 -0400

    LogDatabase, LogViewer are born
    LogDatabase, LogViewer will manage and display historic data

---
+++ src/ca/uwaterloo/syde/shipmate/LogViewer.java
@@ -0,0 +1,5 @@
+package ca.uwaterloo.syde.shipmate;
+
+public class LogViewer {
+
+}
---
+++ src/ca/uwaterloo/syde/shipmate/entity/LogDatabase.java
@@ -0,0 +1,33 @@
+package ca.uwaterloo.syde.shipmate.entity;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class LogDatabase extends SQLiteOpenHelper {
+
+	public LogDatabase(Context context, String name, CursorFactory factory,
+			int version) {
+		super(context, name, factory, version);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+        String CREATE_CONTACTS_TABLE = "CREATE TABLE " + TABLE_CONTACTS + "("
+                + KEY_ID + " INTEGER PRIMARY KEY," + KEY_NAME + " TEXT,"
+                + KEY_PH_NO + " TEXT" + ")";
+        db.execSQL(CREATE_CONTACTS_TABLE);
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // Drop older table if existed
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_CONTACTS);
+ 
+        // Create tables again
+        onCreate(db);
+	}
+
+}
