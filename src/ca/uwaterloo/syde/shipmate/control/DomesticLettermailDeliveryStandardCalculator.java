dcb3307d67672230adbb85b3b4618f645420f72c
Brian Stock <bestock@uwaterloo.ca>
2014-04-04 19:42:27 -0400

    fixed DomesticLettermailDeliveryStandardCalculator constructer
    added context parameter, required for SQLite storage

--- src/ca/uwaterloo/syde/shipmate/control/DomesticLettermailDeliveryStandardCalculator.java
+++ src/ca/uwaterloo/syde/shipmate/control/DomesticLettermailDeliveryStandardCalculator.java
@@ -2,25 +2,40 @@ package ca.uwaterloo.syde.shipmate.control;
 
 import java.util.Date;
 
-import ca.uwaterloo.syde.shipmate.entity.DedicatedProcessingFacility;
+import android.content.Context;
+
+import ca.uwaterloo.syde.shipmate.entity.LogDatabase;
 import ca.uwaterloo.syde.shipmate.entity.LogEntry;
-import ca.uwaterloo.syde.shipmate.entity.NodeDatabase;
 import ca.uwaterloo.syde.shipmate.entity.PostalCode;
 
+/**
+ * DomesticLettermailDeliveryStandardCalculator implements delivery standard calculation for domestic lettermail.
+ * @author Brian Stock
+ *
+ */
 public class DomesticLettermailDeliveryStandardCalculator extends DeliveryStandardCalculator {
 	private PostalCode origin;
 	private PostalCode destination;
 	private static final String SHIPPING_TYPE_KEY = "Domestic Lettermail";
 	private Date date = new Date(System.currentTimeMillis());
+	private Context context;
 	
 	private final int additionalRemoteTime = 4;
+	LogDatabase logDb = new LogDatabase(context);
 	
-	public DomesticLettermailDeliveryStandardCalculator(PostalCode origin, PostalCode destination) {
+	public DomesticLettermailDeliveryStandardCalculator(PostalCode origin, PostalCode destination, Context context) {
 		this.origin = origin;
 		this.destination = destination;
+		this.context = context;
 		deliveryStandard = determineDeliveryStandard(origin, destination);
 	}
 	
+	/**
+	 * determineDeliveryStandard determines delivery standard for an origin, destination pair.
+	 * @param origin
+	 * @param destination
+	 * @return String delivery standard
+	 */
 	private String determineDeliveryStandard(PostalCode origin, PostalCode destination) {
 		String result = "";
 		int totalTime = 0;
@@ -40,21 +55,17 @@ public class DomesticLettermailDeliveryStandardCalculator extends DeliveryStanda
 		}
 		
 		// handle ranged arrival time due to remote PC
-		//Date calculatedArrivalBeginDate = addBusinessDays(baseTime);
-		//Date calculatedArrivalEndDate;
 		if (rangedArrivalDate)
 		{
 			result += " to " + Integer.toString(baseTime + additionalRemoteTime);
-			//calculatedArrivalEndDate = addBusinessDays(totalTime);
 		} else
 		{
-			//calculatedArrivalEndDate = calculatedArrivalBeginDate;
 		}
 
 		// display resulting output
 		result += " business days";
 		LogEntry logEntry = new LogEntry(origin.getKey(), destination.getKey(), SHIPPING_TYPE_KEY, date.toString(), result);
-		
+		logDb.addEntry(logEntry);
 		return result;
 	}
 	
